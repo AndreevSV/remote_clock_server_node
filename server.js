cffac12fddd5605e36443a19fe7a74a1ef7ef19d
AndreevSV <andreevsv@gmail.com>
2025-04-30 12:48:38 +0300

    server

---
+++ server.js
@@ -0,0 +1,12 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world');
+})
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`)
+});
