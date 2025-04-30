84bc6953571ab32f21bb6ba8c8057e863cefe59c
AndreevSV <andreevsv@gmail.com>
2025-04-30 13:00:46 +0300

    change port

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello world');
-})
+});
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`)
+    console.log(`Server started on port: ${PORT}`);
 });
